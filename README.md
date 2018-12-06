528fe77f56a9be39504cbb8e9cc4562cae407ecb
Marcel Jovic <punikonta@gmail.com>
2018-12-06 19:13:42 +0100

    This should demonstrate the issue.

---
+++ README.md
@@ -0,0 +1,38 @@
+# What is this?
+
+A [short, self contained and correct (compilable) example](http://sscce.org/) for an issue.
+
+# What is this trying to show?
+
+TODO: add link to isse
+
+# Installation
+
+```
+git clone https://github.com/num-lock/node-opcua-permission-issue.git
+cd node-opcua-permission-issue
+```
+
+followed by
+
+```
+yarn install
+```
+
+or
+
+```
+npm install
+```
+
+# Getting started
+
+```
+yarn run start
+```
+
+or
+
+```
+npm run start
+```
---
+++ index.js
@@ -0,0 +1,61 @@
+const path = require('path');
+
+const opcua = require('node-opcua');
+
+const server = new opcua.OPCUAServer({
+    port: 12345,
+    nodeset_filename: [
+        path.join(__dirname, 'nodesets', 'Opc.Ua.NodeSet2.xml'),
+        path.join(__dirname, 'nodesets', 'Opc.Ua.Di.NodeSet2.xml'),
+        path.join(__dirname, 'nodesets', 'customnodeset.xml'),
+    ],
+    userManager: {
+        isValidUser: (username, password) => {
+            // for this demonstration just accept all credentials ...
+            return true;
+        },
+        getUserRole: (username) => {
+            // for this demonstration everyone is an admin ...
+            return 'admin';
+        }
+    },
+    allowAnonymous: false
+});
+
+return server.start((err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    const addressSpace = server.engine.addressSpace;
+
+    const node = addressSpace.findNode('ns=3;i=6001');
+    if (node) {
+        node.bindVariable({
+            get: () => {
+                return new opcua.Variant({
+                        dataType: opcua.DataType.Double,
+                        value: Math.random() * 10 + 20
+                    }
+                );
+            },
+            set: (variant) => {
+                console.log('this never gets called anyways :(');
+                return opcua.StatusCodes.Good;
+            },
+            // does this even do anything?
+            userAccessLevel: opcua.makeAccessLevel("CurrentRead | CurrentWrite"),
+            accessLevel: opcua.makeAccessLevel("CurrentRead | CurrentWrite"),
+            permissions: {
+                CurrentRead: ["*"], // everyone
+                CurrentWrite: ["*"] // everyone
+            }
+        }, {overwrite: true});
+    }
+
+    const endpoints = server.endpoints[0].endpointDescriptions();
+    for (let endpoint of endpoints) {
+        console.log(endpoint.endpointUrl, endpoint.securityMode.toString(), endpoint.securityPolicyUri.toString());
+    }
+    return console.log('listening ...');
+});
